70ef5cc9fdb28bff7167fe3779fb832fa5170361
Nguyen Ba Dat <datngba2310@gmail.com>
2026-01-02 13:37:35 +0700

    feat: implement JWT authentication and role-based permissions handling

---
+++ src/configs/corsConfig.js
@@ -0,0 +1,9 @@
+// CORS configuration
+const whitelist = ['http://localhost:5173']
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}
